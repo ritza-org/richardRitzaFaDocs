cd6ac7f7029233a085ff668e57e3f1ae197ff9b0
Gareth Dwyer <gareth@ritza.co>
2026-05-06 13:51:08 +0200

    link fixes

--- astro/src/pages/docs/llms.txt.ts
+++ astro/src/pages/docs/llms.txt.ts
@@ -3,11 +3,25 @@ import type { APIRoute } from "astro";
 
 const docs = await getCollection("docs");
 
+function getDocsUrl(docId: string): string {
+    let path = docId;
+
+    if (path === "index") {
+        return "https://fusionauth.io/docs/";
+    }
+
+    if (path.endsWith("/index")) {
+        path = path.replace(/\/index$/, "");
+    }
+
+    return `https://fusionauth.io/docs/${path}/`;
+}
+
 export const GET: APIRoute = async ({}) => {
     return new Response(
         `## FusionAuth.io Documentation\n\n${docs
             .map((doc) => {
-                return `- [${doc.data.title}](https://fusionauth.io/docs/${doc.slug}/)\n`;
+                return `- [${doc.data.title}](${getDocsUrl(doc.id)})\n`;
             })
             .join("")}`,
         { headers: { "Content-Type": "text/plain; charset=utf-8" } }
